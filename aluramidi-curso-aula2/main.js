ef348278483edb6b5d39316a00d0b42d3eddd11b
sayzuritrevizan <148063702+sayzuritrevizan@users.noreply.github.com>
2023-10-16 09:52:42 -0300

    att

--- aluramidi-curso-aula2/main.js
+++ aluramidi-curso-aula2/main.js
@@ -2,4 +2,10 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listadeTeclas = document.querySelectorAll('.tecla');
+
+
+while(){
+    listadeTeclas[0].onclick = tocaSomPom
+
+}
